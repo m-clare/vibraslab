cde8be060f328df0a1a204a4df1d079d0dcc46e6
Maryanne Wachter <mclare@utsv.net>
2018-11-26 21:07:05 -0500

    working slab frequency calculation

--- two_way_slab.py
+++ two_way_slab.py
@@ -1,11 +1,11 @@
-from numpy import sqrt, pi, roots, array, shape, zeros
+from numpy import sqrt, pi, roots, array, shape, zeros, interp
 
 class TwoWayFlatPlateSlab(object):
 	'''
 	NOTE THIS IS ONLY FOR INTERIOR BAY CURRENTLY WITH CONTINUOUS SPANS
 	'''
 
-	def __init__(self, l_1, l_2, h, f_c, f_y, w_c, col_size, loading=None, reinforcement=None, floor_type='rc'):
+	def __init__(self, l_1, l_2, h, f_c, f_y, w_c, nu, col_size, loading=None, reinforcement=None, floor_type='rc'):
 		self.l_1 = l_1
 		self.l_2 = l_2
 		self.h = h
@@ -15,6 +15,7 @@ class TwoWayFlatPlateSlab(object):
 		self.l_ratio = l_1 / l_2
 		self.f_y = f_y
 		self.w_c = w_c
+		self.nu  = nu # Poisson's ratio
 		self.loading = loading or {}
 		self.reinforcement = reinforcement or {}
 
@@ -24,7 +25,7 @@ class TwoWayFlatPlateSlab(object):
 		else:
 			raise ValueError
 
-		if floor_type =='rc':
+		if floor_type == 'rc':
 			self.E_c *= 1.2
 
 		if w_c == 150.0:
@@ -37,10 +38,35 @@ class TwoWayFlatPlateSlab(object):
 		self.n = 29000000 / self.E_c
 		self.f_r = 4.5 * self.lambda_cw * sqrt(f_c)
 
+		# set strip widths
+		self.strips = {'l_1': {}, 'l_2': {}}
+		col_width   = min(0.25 * self.l_1, 0.25 * self.l_2)
+		self.strips['l_1']['column'] = col_width * 2.
+		self.strips['l_2']['column'] = col_width * 2.
+		self.strips['l_1']['middle'] = self.l_1 - col_width * 2.
+		self.strips['l_2']['middle'] = self.l_2 - col_width * 2.
+
+	def calculate_Mn(self, strip_type, location, M_0):
+		if strip_type == 'middle' and location == 'p':
+			p_moment    = 0.35
+			dist_factor = 0.4
+		elif strip_type == 'middle' and location == 'n':
+			p_moment    = 0.65
+			dist_factor = 0.25
+		elif strip_type == 'column' and location == 'p':
+			p_moment    = 0.35
+			dist_factor = 0.6
+		elif strip_type == 'column' and location == 'n':
+			p_moment    = 0.65
+			dist_factor = 0.75
+		else:
+			raise NameError
+		return round(p_moment * dist_factor * M_0 * 0.001, 1)
+
 	def calculate_bending_moments(self, span, col_dim):
 
 		def factor_moment(dead, live):
-			return 1.2 * dead + 1.6 * live
+			return round(1.2 * dead + 1.6 * live, 1)
 
 		span_types = ['l_1', 'l_2']
 		if span not in span_types:
@@ -63,21 +89,21 @@ class TwoWayFlatPlateSlab(object):
 					'factored': {'column': {'n': 0.0, 'p': 0.0}, 
 								'middle': {'n': 0.0, 'p': 0.0}}}
 		service = load_out['service']
-		# Service moments
-		service['column']['n']['dl'] = round(0.65 * M_0_dl * 0.75 * 0.001, 1)
-		service['middle']['n']['dl'] = round(0.65 * M_0_dl * 0.25 * 0.001, 1)
-		service['column']['n']['ll'] = round(0.65 * M_0_ll * 0.75 * 0.001, 1)
-		service['middle']['n']['ll'] = round(0.65 * M_0_ll * 0.25 * 0.001, 1)
-		service['column']['p']['dl'] = round(0.35 * M_0_dl * 0.6 * 0.001, 1)
-		service['column']['p']['ll'] = round(0.35 * M_0_ll * 0.6 * 0.001, 1)
-		service['middle']['p']['dl'] = round(0.35 * M_0_dl * 0.4 * 0.001, 1)
-		service['middle']['p']['ll'] = round(0.35 * M_0_ll * 0.4 * 0.001, 1)
+		strip_types = ['column', 'middle']
+		moments = ['p', 'n']
+		load_type = ['dl', 'll']
+		for strip in strip_types:
+			for moment in moments:
+				for load in load_type:
+					if load == 'dl':
+						service[strip][moment][load] = self.calculate_Mn(strip, moment, M_0_dl)
+					if load == 'll':
+						service[strip][moment][load] = self.calculate_Mn(strip, moment, M_0_ll)
 		# Factored moments
 		factored = load_out['factored']
-		factored['column']['n'] = round(factor_moment(service['column']['n']['dl'], service['column']['n']['ll']), 1)
-		factored['column']['p'] = round(factor_moment(service['column']['p']['dl'], service['column']['p']['ll']), 1)
-		factored['middle']['n'] = round(factor_moment(service['middle']['n']['dl'], service['middle']['n']['ll']), 1)
-		factored['middle']['p'] = round(factor_moment(service['middle']['p']['dl'], service['middle']['p']['ll']), 1)
+		for strip in strip_types:
+			for moment in moments:
+				factored[strip][moment] = factor_moment(service[strip][moment]['dl'], service[strip][moment]['ll'])
 		return load_out
 
 	def calculate_flexural_reinforcement(self, bending_moment, cover=0.75, bar_guess=0.75):
@@ -95,29 +121,26 @@ class TwoWayFlatPlateSlab(object):
 		return 1 / 12. * strip_width * 12 * self.h ** 3.0
 
 	def calculate_strip_I_e(self, strip_width, M_n, As, d=None):
-		n = self.n
-		h = self.h
-		f_r = self.f_r
 		# calculate I_g 
 		I_g = self.calculate_strip_I_g(strip_width) # does not account for steel reinf. (conservative)
-		y_t = h / 2.0
-		a_j = strip_width * 12 / (n * As)
+		y_t = self.h / 2.0
+		a_j = strip_width * 12 / (self.n * As)
 		# estimate d if not provided
 		if d == None:
 			cover = 0.75
 			bar_guess = 1.0
-			d = h - cover - 0.5 * bar_guess
+			d = self.h - cover - 0.5 * bar_guess
 		p    = zeros(3)
 		p[0] = strip_width * 12 * 0.5
-		p[1] = n * As
-		p[2] = -n * As * d
+		p[1] = self.n * As
+		p[2] = -self.n * As * d
 		kd   = max(roots(p)) # error handling??
-		I_cr = strip_width * 12 * kd ** 3.0 / 3 + n * As * (d - kd) ** 2.0
+		I_cr = strip_width * 12 * kd ** 3.0 / 3 + self.n * As * (d - kd) ** 2.0
 		I_cr = min(I_cr, I_g)
-		M_cr = f_r * I_g / y_t * 1 / 12000. # ft-kips
+		M_cr = self.f_r * I_g / y_t * 1 / 12000. # ft-kips
 		M_ratio = M_cr / M_n
 		if M_ratio > 1.0:
-			I_e = I_g
+			I_e = I_g # uncracked section
 		else:
 			I_e = I_cr / (1 - (M_ratio ** 2.0 * (1 - I_cr / I_g)))
 		return round(I_e, 0)
@@ -142,17 +165,16 @@ class TwoWayFlatPlateSlab(object):
 		bending_moment = self.calculate_bending_moments(span, col_dim)
 		bm_p = sum(bending_moment['service'][strip_type]['p'].values())
 		bm_n = sum(bending_moment['service'][strip_type]['n'].values())
-		#print(span, strip_type, bm_n, bm_p)
 		reinf_p = self.reinforcement[span][strip_type]['p']
 		reinf_n = self.reinforcement[span][strip_type]['n']
+		# I_m  = self.calculate_strip_I_e(self.strips[span][strip_type], bm_p, reinf_p)
+		# I_e1 = self.calculate_strip_I_e(self.strips[span][strip_type], bm_n, reinf_n)
+		# print(I_m)
+		# print(I_e1)
 		if strip_type == 'column':
-			#print(col_width, bm_p, reinf_p)
-			#print(col_width, bm_n, reinf_n)
 			I_m  = self.calculate_strip_I_e(col_width, bm_p, reinf_p)
 			I_e1 = self.calculate_strip_I_e(col_width, bm_n, reinf_n)
 		if strip_type == 'middle':
-			#print(middle_width, bm_p, reinf_p)
-			#print(middle_width, bm_n, reinf_n)
 			I_m  = self.calculate_strip_I_e(middle_width, bm_p, reinf_p)
 			I_e1 = self.calculate_strip_I_e(middle_width, bm_n, reinf_n)
 		I_e2 = I_e1 # when would this differ?
@@ -188,53 +210,55 @@ class TwoWayFlatPlateSlab(object):
 			self.I_e = ((I_e_csx + I_e_msy) + (I_e_csy + I_e_msx)) / 2.0
 		return self.I_e
 
+	def calculate_panel_I_g(self):
+		cs_1, ms_1 = self.get_strip_widths('l_1')
+		cs_2, ms_2 = self.get_strip_widths('l_2')
+		I_g_csx = self.calculate_strip_I_g(cs_1)
+		I_g_csy = self.calculate_strip_I_g(cs_2)
+		I_g_msx = self.calculate_strip_I_g(ms_1)
+		I_g_msy = self.calculate_strip_I_g(ms_2)
+		self.I_g = ((I_g_csx + I_g_msy) + (I_g_csy + I_g_msx)) / 2.0
+		return self.I_g
+
 	def calculate_k_1(self):
 		# accounts for level of cracking
-		if self.rho <= 0.01:
-			#  low reinforcement ratio (should include temp/shrink reinforcement)
-			I_e = min(I_cr / (1 - (M_cr / M_a) ** 2.0 * (1 - I_cr / I_g)), I_g)
-		elif l_ratio == 1.0:
+		# if self.rho <= 0.01:
+		# 	#  low reinforcement ratio (should include temp/shrink reinforcement)
+		# 	I_e = min(I_cr / (1 - (M_cr / M_a) ** 2.0 * (1 - I_cr / I_g)), I_g)
+		if self.l_ratio == 1.0:
 			# square panel
 			I_e = 0.7 * I_m + 0.15 * (I_e1 + I_e2)
 		else: 
-			# rectangular panel
-			I_e = ((I_e_csx + I_e_msy) + (I_e_csy + I_e_msx)) / 2.0
-
-		self.k_1 = I_e / I_g
+			I_e = self.calculate_panel_I_e()
+			I_g = self.calculate_panel_I_g()
+			self.k_1 = I_e / I_g
 		return self.k_1
 
 	def calculate_f_i(self):
 		# calculate k_1 - accounts for level of cracking
-		k_1 = self.calculate_k_1
+		k_1 = self.calculate_k_1()
 		# calculate k_2
-		if self.col_size > 24:
+		col_size = max(self.c_b, self.c_h)
+		if col_size > 24:
 			k_2 = 2.1
 		else:
 			k_2 = 1.9
-		# calculate lambda_i_sq
-		if l_ratio == 1.0:
-			lambda_i_sq = 7.12
-		elif l_ratio == 1.5:
-			lambda_i_sq = 8.92
-		elif l_ratio == 2.0:
-			lambda_i_sq = 9.29
-		else: 
-			raise NotImplementedError
-		self.f_i = k_2 * lambda_i_sq/ 2 * pi * l_1 ** 2.0 * \
-				   sqrt((k_1 * E_c, h ** 3.0) / (12 * gamma * (1 - nu ** 2.0))) 
-		return self.f_i
+		# calculate lambda_i_sq - allow linear interpolation
+		lambda_i_sq = interp(self.l_ratio, [1.0, 1.5, 2.0], [7.12, 8.92, 9.29])
+		sdl = self.loading['sdl']
+		ll  = self.loading['ll_vib']
+		mass = (((self.h / 12. * self.w_c) + (sdl + ll)) * self.l_1 * self.l_2) / 32.2 # lb sec2/ft
+		gamma = mass / (self.l_1 * self.l_2) # slug / ft2
+		self.f_i = k_2 * lambda_i_sq / (2 * pi * self.l_1 ** 2.0) * \
+				   (k_1 * self.E_c * 144 * (self.h / 12.) ** 3.0 / (12 * gamma * (1 - self.nu ** 2.0))) ** 0.5
+		return round(self.f_i, 1)
 
 if __name__ == "__main__":
 	loading = {'sdl': 20., 'll_design': 65., 'll_vib': 11.}
 	reinf   = {'l_1': {'column': {'n': 5.39, 'p': 2.31}, 'middle': {'n': 2.05, 'p': 2.05}},
 			   'l_2': {'column': {'n': 4.15, 'p': 2.05}, 'middle': {'n': 3.08, 'p': 3.08}}}
-	test = TwoWayFlatPlateSlab(l_1=25.0, l_2=20.0, h=9.5, f_c=4000, f_y=60000, w_c=150, col_size={'b': 22., 'h': 22.}, loading=loading, reinforcement=reinf)
-	bm_1 = test.calculate_bending_moments(span='l_1', col_dim=22.0)
-	M_1n = sum(bm_1['service']['column']['n'].values())
-	M_1p = sum(bm_1['service']['column']['p'].values())
-	bm_2 = test.calculate_bending_moments(span='l_2', col_dim=22.0)
-	M_2n = sum(bm_2['service']['column']['n'].values())
-	M_2p = sum(bm_2['service']['column']['p'].values())
-	print(test.calculate_panel_I_e())
+	test = TwoWayFlatPlateSlab(l_1=25.0, l_2=20.0, h=9.5, f_c=4000, f_y=60000, w_c=150, nu=0.2, col_size={'b': 22., 'h': 22.}, 
+		 				       loading=loading, reinforcement=reinf)
+	print(test.calculate_f_i())
 
 
