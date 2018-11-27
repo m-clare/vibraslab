0d0868587c4c17a5bbbaf80fa56f60896a0810b0
m-clare <m-clare@users.noreply.github.com>
2018-11-27 13:00:47 -0500

    working vibration for concrete and cleaned up two way flat plate

--- check_vib.py
+++ check_vib.py
@@ -1,5 +1,8 @@
-from numpy import pi, sin
+from numpy import pi, sin, exp, interp, sqrt
 
+vibration_crit_limits = {'Ordinary workshops': 32000, 'Offices': 16000, 'Computer systems': 8000, 'Residences': 8000,
+						 'Operating rooms': 4000, 'Surgery facilities': 4000, 'Bench microscopes 100x': 4000, 'Lab robots': 4000,
+						 'Bench microscopes 400x': 2000}
 
 def calculate_mode_scaling(x, y, Lb, Lg, fb, fg):
 	if fb <= fg:
@@ -8,4 +11,96 @@ def calculate_mode_scaling(x, y, Lb, Lg, fb, fg):
 		phi = sin(pi * (x + Lb) / (3 * Lb)) * sin(pi * y / Lg)
 	return phi
 
-def calculate_
+def check_sensitive_equip_steel(floor_fn, eff_weight, damping, manufacturer_limit=None, limit_type=None, limit_app=None):
+	# steel
+	walking_parameters = {'very_slow': {'f_step': 1.25}, 
+					  	  'slow': {'f_step': 1.6, 'f_4max': 6.8, 'f_L': 6., 'f_U': 8, 'gamma': 0.10},
+					  	  'moderate': {'f_step': 1.85, 'f_4max': 8.0, 'f_L': 7., 'f_U': 9, 'gamma': 0.09},
+					  	  'fast': {'f_step': 2.1, 'f_4max': 8.8, 'f_L': 8., 'f_U': 10, 'gamma': 0.08}}
+
+	limit_types = ['one-third octave velocity', 'one-third octave acceleration', 'peak velocity', 'peak acceleration', 
+				   'narrowband spectral velocity', 'narrowband spectral acceleration']
+
+	def eqn_6_3a(f_step, f_n, beta, W):
+		V_13 = 250e6 / (beta * W * 1000) * f_step ** 2.43 / f_n ** 1.8 * (1 - exp(-2 * pi * beta * f_n / f_step))
+		return round(V_13, 0)
+
+	def eqn_6_3b(f_step, f_n, beta, gamma, W):
+		V_13 = 175e6 / (beta * W * 1000 * sqrt(f_n)) * exp(-gamma * f_n) # Eqn 6-3b
+		return round(V_13, 0)
+
+	if not limit_type in limit_types:
+		raise NameError
+	if manufacturer_limit:
+		limit = manufacturer_limit
+	else:
+		limit = vibration_crit_limits[limit_app]
+	if limit_type == 'one-third octave velocity':
+		V_13_out = {}
+		for speed in walking_parameters:
+			# Check if low or high frequency floor
+			f_step = walking_parameters[speed]['f_step']
+			if 'f_4max' in walking_parameters[speed]:
+				f_4max = walking_parameters[speed]['f_4max']
+				# slow/moderate/fast
+				f_L = walking_parameters[speed]['f_L']
+				f_U = walking_parameters[speed]['f_U']
+				gamma = walking_parameters[speed]['gamma']
+				if floor_fn <= f_L:
+					V_13 = eqn_6_3b(f_step, floor_fn, damping, gamma, eff_weight)
+				elif floor_fn >= f_U:
+					V_13 = eqn_6_3a(f_step, floor_fn, damping, eff_weight)
+				else: # linear interpolate between equations
+					V_13_f_L = eqn_6_3b(f_step, f_L, damping, gamma, eff_weight)
+					V_13_f_U = eqn_6_3a(f_step, f_U, damping, eff_weight)
+					V_13  = round(interp(floor_fn, [f_L, f_U], [V_13_f_U, V_13_f_L]), 0)
+			else:
+				# very slow
+				V_13 = eqn_6_3a(f_step, floor_fn, damping, eff_weight)
+			V_13_out[speed] = V_13
+		return V_13_out
+	elif limit_type == 'peak acceleration':
+		pass
+	elif limit_type == 'narrowband spectral velocity':
+		pass
+	elif limit_type == 'narrowband spectral acceleration':
+		pass
+	else: 
+		raise NameError
+
+def check_sensitive_equip_concrete(floor_fn, delta_p, manufacturer_limit=None, limit_type=None, limit_app=None):
+	# concrete
+	footfall_impulse_parameters = {'fast': {'F_m': 315., 'f_0': 5.0, 'U_v': 25000, 'steps/min': 100.},
+							  	   'moderate': {'F_m': 280., 'f_0': 2.5, 'U_v': 5500, 'steps/min': 75.},
+							 	   'slow': {'F_m': 240., 'f_0': 1.4, 'U_v': 1500, 'steps/min': 50.}}
+
+	if manufacturer_limit:
+		limit = manufacturer_limit
+	else:
+		limit = vibration_crit_limits[limit_app]
+	if limit_type == 'maximum velocity':
+		V_out = {}
+		for pace in footfall_impulse_parameters:
+			parameters = footfall_impulse_parameters[pace]
+			f_0 = parameters['f_0']
+			if floor_fn / f_0 > 1.0: # should be >> 0.5
+				U_v = parameters['U_v']
+				V = delta_p * U_v / floor_fn
+			else:
+				raise ValueError
+			V_out[pace] = round(V * 1e6, 0) # provide in micro-in/sec (mips)
+		return V_out
+
+if __name__ == "__main__":
+	from two_way_slab import TwoWayFlatPlateSlab
+	# l_1 must be longer span, l_2 must be shorter span
+	loading = {'sdl': 20., 'll_design': 65., 'll_vib': 11.}
+	reinf   = {'l_1': {'column': {'n': 5.39, 'p': 2.31}, 'middle': {'n': 2.05, 'p': 2.05}},
+			   'l_2': {'column': {'n': 4.15, 'p': 2.05}, 'middle': {'n': 3.08, 'p': 3.08}}}
+	floor = TwoWayFlatPlateSlab(l_1=25.0, l_2=20.0, h=9.5, f_c=4000, f_y=60000, w_c=150, nu=0.2, col_size={'b': 22., 'h': 22.}, 
+		 				       loading=loading, reinforcement=reinf)
+	fn = floor.calculate_f_i()
+	delta_p = floor.calculate_delta_p()
+
+	print(check_sensitive_equip_concrete(8.6, 4.659e-6, manufacturer_limit=None, limit_type='maximum velocity', limit_app='Operating rooms')) # does not allow for accounting for location!
+	print(check_sensitive_equip_concrete(fn, delta_p, manufacturer_limit=None, limit_type='maximum velocity', limit_app='Operating rooms')) # does not allow for accounting for location!
--- two_way_slab.py
+++ two_way_slab.py
@@ -1,4 +1,4 @@
-from numpy import sqrt, pi, roots, array, shape, zeros, interp
+from numpy import sqrt, pi, roots, array, shape, zeros, interp, tanh, cosh
 
 class TwoWayFlatPlateSlab(object):
 	'''
@@ -6,17 +6,13 @@ class TwoWayFlatPlateSlab(object):
 	'''
 
 	def __init__(self, l_1, l_2, h, f_c, f_y, w_c, nu, col_size, loading=None, reinforcement=None, floor_type='rc'):
-		if l_2 > l_1:
-			self.l_1 = l_2
-			self.l_2 = l_1 
-		else:
-			self.l_1 = l_1
-			self.l_2 = l_2
+		self.l_1 = l_1
+		self.l_2 = l_2
 		self.h = h
 		self.f_c = f_c
-		self.c_b = col_size['b']
-		self.c_h = col_size['h']
-		self.l_ratio = l_1 / l_2
+		self.c_1 = col_size['c1']
+		self.c_2 = col_size['c2']
+		self.l_ratio = self.l_1 / self.l_2
 		self.f_y = f_y
 		self.w_c = w_c
 		self.nu  = nu # Poisson's ratio
@@ -44,13 +40,22 @@ class TwoWayFlatPlateSlab(object):
 
 		# set strip widths
 		self.strips = {'l_1': {}, 'l_2': {}}
-		col_width   = min(0.25 * self.l_1, 0.25 * self.l_2)
-		self.strips['l_1']['column'] = col_width * 2.
-		self.strips['l_2']['column'] = col_width * 2.
-		self.strips['l_1']['middle'] = self.l_1 - col_width * 2.
-		self.strips['l_2']['middle'] = self.l_2 - col_width * 2.
+		col_width   = min(0.25 * self.l_1, 0.25 * self.l_2) * 2.
+		self.strips['l_1']['column'] = col_width 
+		self.strips['l_2']['column'] = col_width 
+		self.strips['l_1']['middle'] = self.l_2 - col_width 
+		self.strips['l_2']['middle'] = self.l_1 - col_width 
+
+		# set mass / weight
+		sdl = self.loading['sdl'] or 0.0
+		ll  = self.loading['ll_design'] or 0.0
+		self.mass = (((self.h / 12. * self.w_c) + (sdl + ll)) * self.l_1 * self.l_2) / 32.2 # lb sec2/ft
+		self.weight = self.mass * 32.2 # lb
 
 	def calculate_Mn(self, strip_type, location, M_0):
+		'''
+		Calculates design moment based on continuous interior span
+		'''
 		if strip_type == 'middle' and location == 'p':
 			p_moment    = 0.35
 			dist_factor = 0.4
@@ -67,7 +72,7 @@ class TwoWayFlatPlateSlab(object):
 			raise NameError
 		return round(p_moment * dist_factor * M_0 * 0.001, 1)
 
-	def calculate_bending_moments(self, span, col_dim):
+	def calculate_bending_moments(self, span):
 
 		def factor_moment(dead, live):
 			return round(1.2 * dead + 1.6 * live, 1)
@@ -75,6 +80,11 @@ class TwoWayFlatPlateSlab(object):
 		span_types = ['l_1', 'l_2']
 		if span not in span_types:
 			raise ValueError("Invalid span type. Expected one of: %s" % span_types)
+
+		if span == 'l_1':
+			col_dim = self.c_1
+		if span == 'l_2':
+			col_dim = self.c_2
 		w_sdl = self.loading['sdl']
 		w_ll_design  = self.loading['ll_design']
 		w_ll_vib = self.loading['ll_vib']
@@ -91,7 +101,7 @@ class TwoWayFlatPlateSlab(object):
 		load_out = {'service': {'column': {'n': {}, 'p': {}}, 
 								'middle': {'n': {}, 'p': {}}},
 					'factored': {'column': {'n': 0.0, 'p': 0.0}, 
-								'middle': {'n': 0.0, 'p': 0.0}}}
+								'middle':  {'n': 0.0, 'p': 0.0}}}
 		service = load_out['service']
 		strip_types = ['column', 'middle']
 		moments = ['p', 'n']
@@ -113,20 +123,15 @@ class TwoWayFlatPlateSlab(object):
 	def calculate_flexural_reinforcement(self, bending_moment, cover=0.75, bar_guess=0.75):
 		raise NotImplementedError("Not implemented (yet)! Use another program to design slab for flexure for a tension controlled section")
 
-	def get_strip_widths(self, span):
-		col_width = min(0.25 * self.l_1, 0.25 * self.l_2) * 2
-		if span == 'l_1':
-			middle_width = self.l_2 - col_width
-		if span == 'l_2':
-			middle_width = self.l_1 - col_width
-		return col_width, middle_width
-
 	def calculate_strip_I_g(self, strip_width):
+		'''
+		Calculates gross moment of inertia neglecting rebar
+		'''
 		return 1 / 12. * strip_width * 12 * self.h ** 3.0
 
 	def calculate_strip_I_e(self, strip_width, M_n, As, d=None):
 		# calculate I_g 
-		I_g = self.calculate_strip_I_g(strip_width) # does not account for steel reinf. (conservative)
+		I_g = self.calculate_strip_I_g(strip_width)
 		y_t = self.h / 2.0
 		a_j = strip_width * 12 / (self.n * As)
 		# estimate d if not provided
@@ -160,53 +165,23 @@ class TwoWayFlatPlateSlab(object):
 		strip_options = ['column', 'middle']
 		if strip_type not in strip_options:
 			raise ValueError
-		if span == 'l_1':
-			col_width, middle_width = self.get_strip_widths('l_1')
-			col_dim = self.c_b
-		if span == 'l_2':
-			col_width, middle_width = self.get_strip_widths('l_2')
-			col_dim = self.c_h
-		bending_moment = self.calculate_bending_moments(span, col_dim)
+		bending_moment = self.calculate_bending_moments(span)
 		bm_p = sum(bending_moment['service'][strip_type]['p'].values())
 		bm_n = sum(bending_moment['service'][strip_type]['n'].values())
 		reinf_p = self.reinforcement[span][strip_type]['p']
 		reinf_n = self.reinforcement[span][strip_type]['n']
-		# I_m  = self.calculate_strip_I_e(self.strips[span][strip_type], bm_p, reinf_p)
-		# I_e1 = self.calculate_strip_I_e(self.strips[span][strip_type], bm_n, reinf_n)
-		# print(I_m)
-		# print(I_e1)
-		if strip_type == 'column':
-			I_m  = self.calculate_strip_I_e(col_width, bm_p, reinf_p)
-			I_e1 = self.calculate_strip_I_e(col_width, bm_n, reinf_n)
-		if strip_type == 'middle':
-			I_m  = self.calculate_strip_I_e(middle_width, bm_p, reinf_p)
-			I_e1 = self.calculate_strip_I_e(middle_width, bm_n, reinf_n)
+		I_m  = self.calculate_strip_I_e(self.strips[span][strip_type], bm_p, reinf_p)
+		I_e1 = self.calculate_strip_I_e(self.strips[span][strip_type], bm_n, reinf_n)
 		I_e2 = I_e1 # when would this differ?
 		I_e  = 0.7 * I_m + 0.15 * (I_e1 + I_e2) # Eqn 4.19
 		return I_e
 
 	def calculate_panel_I_e(self):
-		if self.l_ratio == 1.0:
-			raise NotImplementedError
-			# # middle strip span l_1
-			# col_width, middle_width = self.get_strip_widths('l_1') # should be identical in both directions
-			# bm_1   = self.calculate_bending_moments(span='l_1', col_dim=self.c_b)
-			# bm_p   = sum(bm_1['service']['middle']['p'].values())
-			# bm_n   = sum(bm_1['service']['middle']['n'].values())
-			# I_m_m  = self.calculate_strip_I_e(middle_width, bm_p, l_1r['middle']['p'])
-			# I_e1_m = self.calculate_strip_I_e(middle_width, bm_n, l_1r['middle']['n'])
-			# I_e2_m = I_e1_m
-			# I_e_m  = 0.7 * I_m_m + 0.15 * (I_e1_m + I_e2_m)
-			# # column strip span l_2
-			# bm_2   = self.calculate_bending_moments(span='l_2', col_dim=self.c_h)
-			# bm_p   = sum(bm_1['service']['column']['p'].values())
-			# bm_n   = sum(bm_1['service']['column']['n'].values())
-			# I_m_m  = self.calculate_strip_I_e(col_width, bm_p, l_1r['column']['p'])
-			# I_e1_c = self.calculate_strip_I_e(col_width, bm_n, l_1r['column']['n'])
-			# I_e2_c = self.calculate_strip_I_e(col_width, bm_n, l_1r['column']['n']) # what differentiates e1 and e2?
-			# I_e_c  = 0.7 * I_m_m + 0.15 * (I_e1_m + I_e2_m)
-			# self.I_e = I_e_m + I_e_c
-		else:
+		if self.l_ratio == 1.0: # square
+			I_e_cs = self.calculate_avg_strip_I_e('l_1', 'column')
+			I_e_ms = self.calculate_avg_strip_I_e('l_2', 'middle') # orthogonal to column strip
+			self.I_e = I_e_cs + I_e_ms
+		else: # rectangular
 			I_e_csx = self.calculate_avg_strip_I_e('l_1', 'column')
 			I_e_csy = self.calculate_avg_strip_I_e('l_2', 'column')
 			I_e_msx = self.calculate_avg_strip_I_e('l_1', 'middle')
@@ -215,34 +190,27 @@ class TwoWayFlatPlateSlab(object):
 		return self.I_e
 
 	def calculate_panel_I_g(self):
-		cs_1, ms_1 = self.get_strip_widths('l_1')
-		cs_2, ms_2 = self.get_strip_widths('l_2')
-		I_g_csx = self.calculate_strip_I_g(cs_1)
-		I_g_csy = self.calculate_strip_I_g(cs_2)
-		I_g_msx = self.calculate_strip_I_g(ms_1)
-		I_g_msy = self.calculate_strip_I_g(ms_2)
+		I_g_csx = self.calculate_strip_I_g(self.strips['l_1']['column'])
+		I_g_csy = self.calculate_strip_I_g(self.strips['l_2']['column'])
+		I_g_msx = self.calculate_strip_I_g(self.strips['l_1']['middle'])
+		I_g_msy = self.calculate_strip_I_g(self.strips['l_2']['middle'])
 		self.I_g = ((I_g_csx + I_g_msy) + (I_g_csy + I_g_msx)) / 2.0
 		return self.I_g
 
 	def calculate_k_1(self):
 		# accounts for level of cracking
 		# if self.rho <= 0.01:
-		# 	#  low reinforcement ratio (should include temp/shrink reinforcement)
-		# 	I_e = min(I_cr / (1 - (M_cr / M_a) ** 2.0 * (1 - I_cr / I_g)), I_g)
-		if self.l_ratio == 1.0:
-			# square panel
-			I_e = 0.7 * I_m + 0.15 * (I_e1 + I_e2)
-		else:
-			I_e = self.calculate_panel_I_e()
-			I_g = self.calculate_panel_I_g()
-			self.k_1 = I_e / I_g
+		#  low reinforcement ratio (should include temp/shrink reinforcement)
+		I_e = self.calculate_panel_I_e()
+		I_g = self.calculate_panel_I_g()
+		self.k_1 = I_e / I_g
 		return self.k_1
 
 	def calculate_f_i(self):
 		# calculate k_1 - accounts for level of cracking
 		k_1 = self.calculate_k_1()
 		# calculate k_2
-		col_size = max(self.c_b, self.c_h)
+		col_size = max(self.c_1, self.c_2)
 		if col_size > 24:
 			k_2 = 2.1
 		else:
@@ -251,17 +219,37 @@ class TwoWayFlatPlateSlab(object):
 		lambda_i_sq = interp(self.l_ratio, [1.0, 1.5, 2.0], [7.12, 8.92, 9.29])
 		sdl = self.loading['sdl']
 		ll  = self.loading['ll_vib']
-		mass = (((self.h / 12. * self.w_c) + (sdl + ll)) * self.l_1 * self.l_2) / 32.2 # lb sec2/ft
-		gamma = mass / (self.l_1 * self.l_2) # slug / ft2
+		gamma = self.mass / (self.l_1 * self.l_2) # slug / ft2
 		self.f_i = k_2 * lambda_i_sq / (2 * pi * self.l_1 ** 2.0) * \
 				   (k_1 * self.E_c * 144 * (self.h / 12.) ** 3.0 / (12 * gamma * (1 - self.nu ** 2.0))) ** 0.5
 		return round(self.f_i, 1)
 
+	def calculate_delta_p(self):
+		# Timoshenko plate theory, maximum deflection of floor system subjected to concentrated unit load
+		# assumes simply supported edges
+		k_1 = self.calculate_k_1()
+		const_p = 6 * (self.l_2 * 12.) ** 2. * (1 - self.nu ** 2.) / (k_1 * self.E_c * self.h ** 3. * pi ** 3.)
+		tol = 1.0
+		i = 1
+		sum_p = 0.
+		m = 1
+		while tol > 0.001:
+			old_sum = sum_p
+			alpha_m = m * pi / 2 * self.l_ratio
+			sum_p += 1 / m ** 3. * (tanh(alpha_m) - alpha_m / (cosh(alpha_m)) ** 2.)
+			tol = abs(sum_p - old_sum)
+			m += 2
+		self.delta_p = const_p * sum_p
+		return self.delta_p
+
+
 if __name__ == "__main__":
+	# l_1 must be longer span, l_2 must be shorter span
 	loading = {'sdl': 20., 'll_design': 65., 'll_vib': 11.}
 	reinf   = {'l_1': {'column': {'n': 5.39, 'p': 2.31}, 'middle': {'n': 2.05, 'p': 2.05}},
 			   'l_2': {'column': {'n': 4.15, 'p': 2.05}, 'middle': {'n': 3.08, 'p': 3.08}}}
-	test = TwoWayFlatPlateSlab(l_1=25.0, l_2=20.0, h=9.5, f_c=4000, f_y=60000, w_c=150, nu=0.2, col_size={'b': 22., 'h': 22.}, 
+	test = TwoWayFlatPlateSlab(l_1=25.0, l_2=20.0, h=9.5, f_c=4000, f_y=60000, w_c=150, nu=0.2, col_size={'c1': 22., 'c2': 22.}, 
 		 				       loading=loading, reinforcement=reinf)
+	print(test.calculate_f_i())
 
 
